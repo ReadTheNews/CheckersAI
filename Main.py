b67f22207120f66665ee88a294babaeb961d2654
ReadTheNews <jamesrdevinney@gmail.com>
2017-12-20 20:56:45 -0600

    Test

--- Main.py
+++ Main.py
@@ -1,2 +1,28 @@
-import tensorflow
+# import tensorflow as tf
+# import keras
+from keras.models import Sequential     # One Layer after the other
+from keras.layers import Dense          # Dense layers are fully connected layers
+import numpy as np
 
+# import collections import deque -- storing moves????
+
+import sys
+sys.version
+print("Hello World")
+
+
+
+# Create network. Input is two consecutive game states, output is Q-values of the possible moves.
+model = Sequential()
+model.add(Dense(64, input_shape=(2,), init='uniform', activation='relu'))
+model.add(Dense(40, init='uniform', activation='relu'))
+model.add(Dense(20, init='uniform', activation='relu'))
+model.add(Dense(env.action_space.n, init='uniform', activation='linear'))
+    # same number of outputs as possible actions
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+# Parameters
+observ_t = 6000        # Number of time-steps we will be acting on the game and observing results
+epsilon = 0.7          # Prob of doing a random move
+gamma = 0.9            # Discounted future reward. How much we care about steps further in time
+mb_size = 300          # Learning mini-batch size
